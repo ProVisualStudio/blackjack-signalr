bcce7296c74334e690e3fca8f059ba4377dc9250
Patrick <p.jopiti@gmail.com>
2017-04-25 08:46:14 +0200

    spostati metodi utilizzati per il deck

--- BlackJack/BlackJack/Hubs/DeckHub.cs
+++ BlackJack/BlackJack/Hubs/DeckHub.cs
@@ -3,19 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
+using BlackJack.Models;
 
 namespace BlackJack.Hubs
 {
     public class DeckHub : Hub
     {
-        public void Hello()
-        {
-            Clients.All.hello();
+
+        /*
+          *Metodo che prende i mazzi di carte, li unisce e i mischia
+          *   
+          */
+        public Deck[] CreateDeck(int qtaMazzi)
+        { 
+            Deck[] decks = new Deck[qtaMazzi];
+            int qtaCarte = qtaMazzi * 52;
+            Card[] cards = new Card[qtaCarte];
+            for (int i = 0; i < qtaMazzi; i++)
+            {
+
+                cards = cards.Union(decks[qtaMazzi].Cards).ToArray();
+            }
+            return decks;
         }
 
-        public void generateDeck()
+        public Card[] ShuffleCards(Card[] cards)
         {
-
+            Random r = new Random();
+            int randomIndex = 0;
+            Card[] shuffleCards = new Card[cards.Length];
+            var listCard = new List<Card>(cards);
+            for (int i = 0; i < cards.Length; i++)
+            {
+                randomIndex = r.Next(0, cards.Length); //Choose a random object in the list
+                shuffleCards[i] = listCard.ElementAt(randomIndex); //add it to the new, random list
+                listCard.RemoveAt(randomIndex); //remove to avoid duplicates
+            }
+            return cards;
         }
     }
 }
--- BlackJack/BlackJack/Hubs/GameHub.cs
+++ BlackJack/BlackJack/Hubs/GameHub.cs
@@ -13,9 +13,9 @@ namespace BlackJack.Hubs
         private static List<User> Users = new List<User>();
         private static Table table = new Table();
         private static int conteggio = 0;
-        private Deck[] decks;
         private Card[] cards;
         private static int qtaMazzi = 6;
+        private DeckHub dh = new DeckHub();
 
         public override Task OnConnected()
         {
@@ -65,38 +65,10 @@ namespace BlackJack.Hubs
          */       
         public void newGame()
         {
-            CreateDeck();
-            ShuffleDeck(this.cards);
+            dh.CreateDeck(qtaMazzi);
+            dh.ShuffleCards(this.cards);
 
         }
-        /*
-         *Metodo che prende i mazzi di carte, li unisce e i mischia
-         *   
-         */
-        public void CreateDeck() {
-            decks = new Deck[qtaMazzi];
-            int qtaCarte = qtaMazzi * 52;
-            cards = new Card[qtaCarte];
-            for (int i = 0; i < qtaMazzi; i++)
-            {
-
-                cards = cards.Union(decks[qtaMazzi].Cards).ToArray();
-            }
-        }
-
-        public void ShuffleDeck(Card[] cards)
-        {
-            Random r = new Random();
-            int randomIndex = 0;
-            Card[] shuffleCards = new Card[cards.Length];
-            var listCard = new List<Card>(cards);
-            for (int i = 0; i < cards.Length; i++)
-            {
-                randomIndex = r.Next(0, cards.Length); //Choose a random object in the list
-                shuffleCards[i] = listCard.ElementAt(randomIndex); //add it to the new, random list
-                listCard.RemoveAt(randomIndex); //remove to avoid duplicates
-            }
-            this.cards = cards;
-        }
+       
     }
 }
